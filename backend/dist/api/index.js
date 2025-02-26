105fb2c634ed94144df56c5a6c326daf7041cecb
shhahad20 <Shhahad20@gmail.com>
2025-02-25 23:58:30 -0800

    deployment

--- backend/dist/api/index.js
+++ backend/dist/api/index.js
@@ -6,7 +6,7 @@ import aiChatRouter from "../src/routers/aiChatRouter.js";
 import apiErrorHandler from '../src/middlewares/errorHandler.js';
 config();
 const app = express();
-const PORT = 3001;
+// const PORT = 3001
 app.use('/public', express.static('public'));
 // app.use(cookieParser())
 // app.use(morgan('dev'))
@@ -20,6 +20,9 @@ app.use(express.json());
 app.use('/api', itemsRouter);
 app.use('/api', aiChatRouter);
 app.use(apiErrorHandler);
-app.listen(PORT, async () => {
-    console.log('Server running http://localhost:' + PORT);
-});
+// app.listen(PORT, async () => {
+//   console.log('Server running http://localhost:' + PORT)
+// }) 
+// For local development you might create a separate file that imports the app and starts the server.
+// For Vercel, simply export the app.
+export default app;
--- backend/dist/src/itemsRouter.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { components, repositories, socialMedia } from './controllers/itemsController';
-const router = express.Router();
-router.get('/repositories', repositories);
-router.get('/components', components);
-router.get('/social-media', socialMedia);
-export default router;
